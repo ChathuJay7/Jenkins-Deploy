a92f4651991b5a3e29e68d8b73de84073099b336
ChathuJay7 <chathurajmhb7@gmail.com>
2024-05-23 17:18:41 +0530

    Initialize commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express()
+
+app.get('/', (req,res) => {
+    res.json({
+        message: "Hello Backend..!"
+    })
+})
+
+app.get('/home', (req,res) => {
+    res.json({
+        message: "Hello To Home..!"
+    })
+})
+
+app.get('/dashboard', (req,res) => {
+    res.json({
+        message: "Welcome to Dahsboard..!"
+    })
+})
+
+app.listen(3000)
